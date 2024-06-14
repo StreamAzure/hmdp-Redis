3065f7ff366bc259d921633b1395771691032c34
StreamAzure <1905105036@qq.com>
2024-06-14 21:18:47 +0800

    一人一单v4: Redis分布式锁，Lua脚本保证删锁原子性，线程ID保证删除的是自己的锁

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,14 +1,26 @@
 package com.hmdp.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
     private String name; // 锁的名称
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
     private StringRedisTemplate stringRedisTemplate;
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+    // 在类加载时初始化脚本，而不是每次释放锁时初始化脚本，提高性能
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -17,14 +29,17 @@ public class SimpleRedisLock implements ILock{
     @Override
     public boolean tryLock(long timeoutSec) {
         String key = KEY_PREFIX + name;
-        String value = Thread.currentThread().getId() + ""; // 以线程ID作为Value
+        String value = ID_PREFIX + Thread.currentThread().getId(); // 以线程ID作为Value
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(key, value, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success); // 如果是Null会自动返回False
     }
 
     @Override
     public void unlock() {
-        // 释放锁
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 调用 lua 脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name), // 获取线程标识
+                ID_PREFIX + Thread.currentThread().getId());
     }
 }
